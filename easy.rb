684fe0f1ef84d0e8f3a7fa2c6d092d08dac8e256
AnasShar <anas10895@gmail.com>
2019-12-02 02:03:59 +0300

    Anas

---
+++ easy.rb
@@ -0,0 +1,202 @@
+students = [
+  {
+      first_name: "Wejdan",
+      last_name: "Alghamdi"
+  },
+  {
+      first_name: "Shahad",
+      last_name: "Bagarish",
+  },
+  {
+      first_name: "Doaa",
+      last_name: "Alalawi",
+  }
+]
+## 1. Return an array of each Student’s full name, upper-cased
+upper_case_full_names = []
+
+for i in students
+    upper_case_full_names.push(i[:first_name].upcase+" " + i[:last_name].upcase
+    )
+
+end
+
+p upper_case_full_names
+
+
+
+
+
+users = [
+  {
+      name: "Mohammad",
+      orders: [
+          {
+              description: "a bmx"
+          }
+      ]
+  },
+  {
+      name: "Ibtissam",
+      orders: [
+          {
+              description: "honey"
+          },
+          {
+              description: "Nintendo 3DS"
+          }
+      ]
+  },
+  {
+      name: "Yazeed",
+      orders: [
+          {
+              description: "a Razer Blade"
+          },
+          {
+              description: "The Arrow DVDs"
+          },
+          {
+              description: "airpods"
+          },
+          {
+              description: "a unicorn"
+          }
+      ]
+  }
+]
+## 2. Find the first order for each user
+for i in users
+    puts i[:orders].first
+end
+
+#_____________________________________
+
+people = [
+  {
+      name: "Ashjan",
+      transactions: [
+          {
+              type: "COFFEE",
+              amount: 7.43
+          },
+          {
+              type: "TACOS",
+              amount: 14.65
+          },
+          {
+              type: "COFFEE",
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: "Abeer",
+      transactions: [
+          {
+              type: "BIKES",
+              amount: 800.00
+          },
+          {
+              type: "TACOS",
+              amount: 14.65
+          },
+          {
+              type: "COFFEE",
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: "Nujud",
+      transactions: [
+          {
+              type: "COFFEE",
+              amount: 7.43
+          },
+          {
+              type: "COFFEE",
+              amount: 100.00
+          },
+          {
+              type: "COFFEE",
+              amount: 4.43
+          }
+      ]
+  }
+]
+## 3. Find the average amount spent on coffee, per transaction, for each person
+
+for i in people do
+    count =0
+    total=0
+    for x in i[:transactions] do
+        if x[:type] == "COFFEE"
+            count += 1
+            total += x[:amount]
+        end
+end
+avg = total/count 
+puts "#{i[:name]}  " "  #{avg}"
+end
+
+
+
+#_____________________________________
+
+
+
+
+stores = [
+  {
+      store_name: "Virgin",
+      products: [
+          {
+              description: "PS4",
+              price: 9384.33
+          },
+          {
+              description: "Pen",
+              price: 345.54
+          }
+      ]
+  },
+  {
+      store_name: "Manuel",
+      products: [
+          {
+              description: "Bread",
+              price: 654.44
+          },
+          {
+              description: "Cheese",
+              price: 323.43
+          }
+      ]
+  },
+  {
+      store_name: "Noon",
+      products: [
+          {
+              description: "iPhone",
+              price: 345.43
+          },
+          {
+              description: "TV",
+              price: 899.33
+          }
+      ]
+  }
+]
+## 4. Find the most expensive product for each store, with the store name:
+for i in stores do
+    product = 0
+
+    for x in i[:products] do
+        if x[:price] > product
+            product = x[:price]
+            puts "#{product} #{i[:store_name]}"
+
+        end
+    end
+end
