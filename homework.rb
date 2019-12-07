65e63b3fa23d66114c118626ebfff6739860216d
iashjan <ashjanot@gmail.com>
2019-12-07 18:34:59 +0300

    ruby homework

---
+++ homework.rb
@@ -0,0 +1,193 @@
+students = [
+  {
+      first_name: "Wejdan",
+      last_name: "Alghamdi"
+  },
+  {
+      first_name: "Shahad",
+      last_name: "Bagarish",
+  },
+  {
+      first_name: "Doaa",
+      last_name: "Alalawi",
+  }
+]
+upper_case_full_names = []
+
+
+
+for names in students do
+    upper_case_full_names.map{|students| [:first_name.upcase] + "." +[:last_name.upcase]}
+  
+  print upper_case_full_names.upcase
+  puts full_names
+
+
+
+
+users = [
+  {
+      name: "Mohammad",
+      orders: [
+          {
+              description: "a bmx"
+          }
+      ]
+  },
+  {
+      name: "Ibtissam",
+      orders: [
+          {
+              description: "honey"
+          },
+          {
+              description: "Nintendo 3DS"
+          }
+      ]
+  },
+  {
+      name: "Yazeed",
+      orders: [
+          {
+              description: "a Razer Blade"
+          },
+          {
+              description: "The Arrow DVDs"
+          },
+          {
+              description: "airpods"
+          },
+          {
+              description: "a unicorn"
+          }
+      ]
+  }
+]
+first_order_for_each_user = users.map{|orders| orders.first }
+print first_order_for_each_user
+
+####################
+
+people = [
+  {
+      name: "Ashjan",
+      transactions: [
+          {
+              type: "COFFEE",
+              amount: 7.43
+          },
+          {
+              type: "TACOS",
+              amount: 14.65
+          },
+          {
+              type: "COFFEE",
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: "Abeer",
+      transactions: [
+          {
+              type: "BIKES",
+              amount: 800.00
+          },
+          {
+              type: "TACOS",
+              amount: 14.65
+          },
+          {
+              type: "COFFEE",
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: "Nujud",
+      transactions: [
+          {
+              type: "COFFEE",
+              amount: 7.43
+          },
+          {
+              type: "COFFEE",
+              amount: 100.00
+          },
+          {
+              type: "COFFEE",
+              amount: 4.43
+          }
+      ]
+  }
+]
+coffee_average_per_person = []
+
+for person in people do
+    people.map{
+
+
+    }
+[
+  {name: "Ashjan", :coffee_average=>5.93},
+  {name: "Abeer", :coffee_average=>4.43},
+  {name: "Nujud", :coffee_average=>37.28666666666667}
+]
+```
+## 4. Find the most expensive product for each store, with the store name:
+```rb
+stores = [
+  {
+      store_name: "Virgin",
+      products: [
+          {
+              description: "PS4",
+              price: 9384.33
+          },
+          {
+              description: "Pen",
+              price: 345.54
+          }
+      ]
+  },
+  {
+      store_name: "Manuel",
+      products: [
+          {
+              description: "Bread",
+              price: 654.44
+          },
+          {
+              description: "Cheese",
+              price: 323.43
+          }
+      ]
+  },
+  {
+      store_name: "Noon",
+      products: [
+          {
+              description: "iPhone",
+              price: 345.43
+          },
+          {
+              description: "TV",
+              price: 899.33
+          }
+      ]
+  }
+]
+most_expensive_products_by_store = []
+
+stores.each{|i| 
+most_expensive_products = i[:products].max_by{|j| j[:price]}
+print most_expensive_product
+
+
+rb
+[
+  {store_name: "Virgin", most_expensive_product: { description: "PS4", price: 9384.33}},
+  {store_name: "Manuel", most_expensive_product: { description: "Bread", price: 654.44}},
+  {store_name: "Noon", most_expensive_product: { description: "TV", price: 899.33}}
+]
+```
